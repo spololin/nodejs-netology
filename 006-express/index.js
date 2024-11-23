947e93a232ef28c835d04595fa85980142f20e33
Sergey Pololin <spololin@gmail.com>
2024-11-23 14:43:53 +0300

    [006] Реализовал задачу

---
+++ 006-express/index.js
@@ -0,0 +1,88 @@
+const express = require("express");
+const { v4: uuid } = require("uuid");
+
+class Book {
+    constructor(data) {
+        this.id = uuid();
+        this.title = data?.title;
+        this.description = data?.description;
+        this.authors = data?.authors;
+        this.favorite = data?.favorite;
+        this.fileCover = data?.fileCover;
+        this.fileName = data?.fileName;
+    }
+}
+
+const store = {
+    books: [new Book(), new Book()],
+};
+
+const app = express();
+app.use(express.json());
+
+app.post("/api/user/login", (req, res) => {
+    res.status(201);
+    res.json({ id: 1, mail: "test@mail.ru" }    );
+});
+
+app.get("/api/books", (req, res) => {
+    const { books } = store;
+    res.json(books);
+});
+
+app.get("/api/books/:id", (req, res) => {
+    const { books } = store;
+    const { id } = req.params;
+    const idx = books.findIndex((el) => el.id === id);
+
+    if (idx !== -1) {
+        res.json(books[idx]);
+    } else {
+        res.status(404);
+        res.json("404 | книга не найдена"); 
+    }
+});
+
+app.post("/api/books/", (req, res) => {
+    const { books } = stor;
+    const { data } = req.body;
+
+    const newBook = new Book(data);
+    books.push(newBook);
+
+    res.status(201);
+    res.json(newBook);
+});
+
+app.put("/api/books/:id", (req, res) => {
+    const { books } = store;
+    const { data } = req.body;
+    const { id } = req.params;
+    const idx = books.findIndex((el) => el.id === id);
+
+    if (idx !== -1) {
+        books[idx] = {
+            ...books[idx],
+            ...data
+        };
+
+        res.json(books[idx]);
+    } else {
+        res.status(404);
+        res.json("Книга не найдена");
+    }
+});
+
+app.delete("/api/books/:id", (req, res) => {
+    const { books } = store;
+    const { id } = req.params;
+    const idx = books.findIndex((el) => el.id === id);
+
+    if (idx !== -1) {
+        books.splice(idx, 1);
+        res.json("ok");
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT);
